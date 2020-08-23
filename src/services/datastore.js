a6a16384ce7103654b94299fb68021a1fcfe9ebe
Devon <devonc2000@gmail.com>
2020-08-22 20:04:29 -0400

    updated firebase database to group account

--- src/services/datastore.js
+++ src/services/datastore.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyBydGIcpkQZWWRzmVCEpdrb4TlxZHl5hlo',
-  authDomain: 'touch-the-fire.firebaseapp.com',
-  databaseURL: 'https://touch-the-fire.firebaseio.com',
-  projectId: 'touch-the-fire',
-  storageBucket: 'touch-the-fire.appspot.com',
-  messagingSenderId: '731232144501',
-  appId: '1:731232144501:web:c45dddd6bd3b4ac1c7fc3c',
+  apiKey: 'AIzaSyD3_y86cNgPL8t7MAYh81rqZBQDcjUSSyI',
+  authDomain: 'touch-the-fire-auth.firebaseapp.com',
+  databaseURL: 'https://touch-the-fire-auth.firebaseio.com',
+  projectId: 'touch-the-fire-auth',
+  storageBucket: 'touch-the-fire-auth.appspot.com',
+  messagingSenderId: '595560193456',
+  appId: '1:595560193456:web:3718b09d84b45319280a75',
 };
 
 firebase.initializeApp(config);
@@ -15,22 +15,3 @@ firebase.initializeApp(config);
 const database = firebase.database().ref('user');
 
 export default database;
-
-// export function fetchUsers(callback) {
-//   database.on('value', (snapshot) => {
-//     const newUserState = snapshot.val();
-//     callback(newUserState);
-//   });
-// }
-
-// export function addUser(user) {
-//   database.push(user);
-// }
-
-// export function deleteUser(id) {
-//   database.child(id).remove();
-// }
-
-// export function updateUser(id, user) {
-//   database.child(id).update(user);
-// }
